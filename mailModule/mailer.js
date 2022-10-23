573490a18d709a9b548e7362ba4dd72001f18318
Deepayan Nandy <deepayan.622@gmail.com>
2022-10-24 01:42:53 +0530

    Update mailer.js

--- mailModule/mailer.js
+++ mailModule/mailer.js
@@ -4,7 +4,7 @@ var transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
     user: 'appsdny@gmail.com',
-    pass: 'lqluebudwozpolck'
+    pass: 'xxxxxxxxxxxxxxxx'
   },
   port:465,
   host:"smtp.gmail.com"
